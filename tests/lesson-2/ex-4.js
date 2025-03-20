91984dee1a1a6746cf8184f2590fd141262c6969
VanDang <vdang264@gmail.com>
2025-03-20 14:44:05 +0700

    fix/fix-loi-logic-ex4

--- tests/lesson-2/ex-4.js
+++ tests/lesson-2/ex-4.js
@@ -1,13 +1,18 @@
 let Height = 166;
-//I use slice to get the last 2 digits but it is only available for type = string so I parse it to string then back to int
-let IdealWeight = (parseInt(Height.toString().slice(-2)) * 9 / 10);
-let MaxWeight = parseInt(Height.toString().slice(-2));
-let MinWeight = (parseInt(Height.toString().slice(-2)) * 8 / 10);
+let Last2Digit;
 
-if (Height <= 200) {
+if (Height <= 100 || Height >= 200) {
+    console.log("Cong thuc chi phu hop voi chieu cao trong khoang 100 den 200");
+} else {
+    Last2Digit = parseInt(Height.toString().slice(-2));
+    //I use slice to get the last 2 digits but it is only available for type = string so I parse it to string then back to int
+
+    let IdealWeight = Last2Digit * 9 / 10;
+    let MaxWeight = Last2Digit;
+    let MinWeight = Last2Digit * 8 / 10;
+
     console.log("Height = " + Height +
         ", IdealWeight = " + IdealWeight +
         ", MaxWeight = " + MaxWeight +
         ", MinWeight = " + MinWeight);
 }
-else { console.log("Cong thuc chi phu hop voi chieu cao nho hon 200") };
