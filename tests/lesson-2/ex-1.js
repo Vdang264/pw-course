7259b928e6a6d9c1519f65232f1dcad029a83490
VanDang <vdang264@gmail.com>
2025-03-20 20:35:48 +0700

    chore/beautify-code

--- tests/lesson-2/ex-1.js
+++ tests/lesson-2/ex-1.js
@@ -4,10 +4,9 @@ let Name = "My number";
 let isEven = false;
 
 //function to check if it is odd or even number
-if (Number % 2 == 0) {isEven = true;}
- else {isEven = false;}
+if (Number % 2 == 0) { isEven = true; }
+else { isEven = false; }
 
 //print the result 
 console.log(`${Name} is even number which is ${isEven}`);
 
- 
