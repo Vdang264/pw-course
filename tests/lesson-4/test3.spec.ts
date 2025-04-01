0d2a5a75a7305dd63cdd18cb79eb818e420d3c4a
VanDang <vdang264@gmail.com>
2025-04-01 16:26:52 +0700

    feat/test-3

---
+++ tests/lesson-4/test3.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test("Add 100 item to Todo list & remove #Odd item", async ({ page }) => {
+    await test.step("Navigate to Home page", async () => {
+        await page.goto("https://material.playwrightvn.com/");
+    });
+
+    await test.step("Navigate to Todo page", async() => {
+        await page.click("//a[@href='03-xpath-todo-list.html']");
+    });
+
+    await test.step("Add 100 item to Todo list", async() => {
+        const inputField = page.locator("//input[@id='new-task']");
+        const button = page.locator("//button[@id='add-task']");
+        for (let i = 1; i <=100; i++) {
+            await inputField.fill(`Item ${i}`);
+            await button.click();
+        };
+    });
+
+    page.on("dialog", async dialog => dialog.accept());
+
+    await test.step("Remove Todo items with Odd#", async () => {
+        for (let i = 1; i <= 100; i += 2) {
+            const xpathOfButton = `//button[@id='${i}-delete']`;
+            const buttonLocator = page.locator(xpathOfButton);
+            
+            if (await buttonLocator.isVisible()) {
+                await buttonLocator.click();
+                await page.waitForTimeout(100); 
+            }
+        }
+    });
+});
